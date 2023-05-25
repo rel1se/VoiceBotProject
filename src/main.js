d77d294e99d60cd02e279326917bc41a7b91dfac
Vyacheslav <botoevslava27032003@mail.ru>
2023-05-25 17:58:08 +0300

    Added logic with OpenAI voice and text messages

--- src/main.js
+++ src/main.js
@@ -1,22 +1,43 @@
-import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
-// import { code } from 'telegraf/format'
+import { code } from 'telegraf/format'
 import config from 'config'
 import process from "nodemon";
 import { ogg } from './ogg.js'
+import { openai } from './openai.js'
+import { removeFile } from './utils.js'
+import { Telegraf, session } from 'telegraf'
+import {initCommand, processTextToChat, INITIAL_SESSION} from "./logic.js";
+
 
 const bot = new Telegraf(config.get('TELEGRAM_BOT'))
 
+// говорим боту, чтобы он использовал session
+bot.use(session())
+
+bot.command('new', initCommand)
+bot.command('start', initCommand)
+
 bot.on(message('voice'), async (ctx) => {
+    ctx.session ??= INITIAL_SESSION
     try {
-        // await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
+        await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
         const userId = String(ctx.message.from.id)
         const oggPath = await ogg.create(link.href, userId)
         const mp3Path = await ogg.toMp3(oggPath, userId)
-
-
-        await ctx.reply(mp3Path)
+        removeFile(oggPath)
+        const text = await openai.transcription(mp3Path)
+        await ctx.reply(code(`Ваш запрос: ${text}`))
+        await processTextToChat(ctx, text)
+    } catch (e) {
+        console.log(`Error while voice message`, e.message)
+    }
+})
+bot.on(message('text'), async (ctx) => {
+    ctx.session ??= INITIAL_SESSION
+    try {
+        await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
+        await processTextToChat(ctx, ctx.message.text)
     } catch (e) {
         console.log(`Error while voice message`, e.message)
     }
